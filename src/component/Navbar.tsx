96bc7da7748dc4b557090683bb87a5eca3273fd4
Thanlwin <armagriyato@gmail.com>
2024-05-22 02:40:27 +0630

    initial commit

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,48 +1,95 @@
-import Link from 'next/link';
-import MaxWidthWrapper from "./MaxWidthWarper";
-import { buttonVariants } from '@/components/ui/button';
-import { LoginLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server';
-import { ArrowRight } from 'lucide-react';
+import {
+  LoginLink,
+  RegisterLink,
+  getKindeServerSession,
+} from "@kinde-oss/kinde-auth-nextjs/server";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
-const Navbar = () => {
+import { buttonVariants } from "@/components/ui/button";
+
+
+import { SOURCE_CODE } from "@/config/links";
+import Image from "next/image";
+import MaxWidthWrapper from "./MaxWidthWarper";
+import { UserAccountNav } from "./UserAccountNav";
+import { MobileNav } from "./MobileNav";
+
+export const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
   return (
-    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+    <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
-        <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
-          <Link href='/' className='flex z-40 font-semibold'>
+        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="flex z-40 font-semibold">
             <span>quill.</span>
           </Link>
 
-          <div className='hidden items-center space-x-4 sm:flex'>
-            <>
-            <Link
-                  href='/Pricing'
-                  className={buttonVariants({
-                    variant: 'ghost',
-                    size: 'sm',
-                  })}>
-                  Pricing
-                </Link>
-                <LoginLink
-                  className={buttonVariants({
-                    variant: 'ghost',
-                    size: 'sm',
-                  })}>
-                  Sign in
-                </LoginLink>
-                <RegisterLink
-                  className={buttonVariants({
-                    size: 'sm',
-                  })}>
-                  Get started{' '}
-                  <ArrowRight className='ml-1.5 h-5 w-5' />
-                </RegisterLink>
-          </>
+          <div className="flex items-center justify-center space-x-4">
+            <MobileNav isAuth={!!user} />
+
+            <div className="hidden sm:flex">
+              {!user ? (
+                <>
+                  <Link
+                    href="/pricing"
+                    className={buttonVariants({
+                      variant: "ghost",
+                      size: "sm",
+                    })}
+                  >
+                    Pricing
+                  </Link>
+
+                  <LoginLink
+                    className={buttonVariants({
+                      variant: "ghost",
+                      size: "sm",
+                    })}
+                  >
+                    Sign in
+                  </LoginLink>
+                  <RegisterLink
+                    className={buttonVariants({
+                      size: "sm",
+                    })}
+                  >
+                    Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+                  </RegisterLink>
+                </>
+              ) : (
+                <>
+                  <Link
+                    href="/dashboard"
+                    className={buttonVariants({
+                      variant: "ghost",
+                      size: "sm",
+                    })}
+                  >
+                    Dashboard
+                  </Link>
+
+                  <UserAccountNav
+                    name={
+                      !user.given_name || !user.family_name
+                        ? "Your Account"
+                        : `${user.given_name} ${user.family_name}`
+                    }
+                    email={user.email ?? ""}
+                    imageUrl={user.picture ?? ""}
+                  />
+                </>
+              )}
+            </div>
+
+            <Link href={SOURCE_CODE} target="_blank" rel="noreferrer noopener">
+              <Image src="/github.svg" alt="GitHub" height={25} width={25} />
+            </Link>
           </div>
         </div>
       </MaxWidthWrapper>
     </nav>
   );
 };
-
-export default Navbar;
---
+++ src/config/links.ts
@@ -0,0 +1,1 @@
+export const SOURCE_CODE = "https://github.com/Leneion405/quill";
