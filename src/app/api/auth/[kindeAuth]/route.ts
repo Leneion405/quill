7bd4b2e4f85c798c955f94996731f7045f734e84
Thanlwin <armagriyato@gmail.com>
2024-05-22 00:38:33 +0630

    initial commit

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,20 +1,10 @@
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
-import { NextRequest, NextResponse } from 'next/server';
+import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
+import { NextRequest } from 'next/server'
 
-export async function GET(request: NextRequest, { params }: { params: { kindeAuth: string } }) {
-  const endpoint = params.kindeAuth;
-
-  try {
-    const result = await handleAuth(request, endpoint);
-
-    // Ensure the result is a Response
-    if (result instanceof Response) {
-      return result;
-    } else {
-      // Handle the case where result is not a Response
-      return NextResponse.json(result);
-    }
-  } catch (error) {
-    return NextResponse.error();
-  }
+export async function GET(
+  request: NextRequest,
+  { params }: any
+) {
+  const endpoint = params.kindeAuth
+  return handleAuth(request, endpoint)
 }
